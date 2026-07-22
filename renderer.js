c4e539ecbe2e5d5e2dd987c49c7160379cc3bd56
webtoolbox <sandeep@websitetoolbox.com>
2026-07-22 19:23:51 +0900

    Use SVG chevron icon for collapsed filtered files
    Matches the PR description toggle icon style.

--- renderer.js
+++ renderer.js
@@ -1457,7 +1457,7 @@ function appendCollapsedFilteredFiles(excludedFiles) {
     </div>
     ${excludedFiles.map((f, i) => `
       <div class="collapsed-file-item" data-index="${i}" title="${f.name}">
-        <span class="toggle-icon">▶</span>
+        <span class="toggle-icon"><svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M6 9l6 6 6-6"/></svg></span>
         <span class="file-path">${f.name}</span>
         <span class="file-ext">${f.ext}</span>
       </div>
